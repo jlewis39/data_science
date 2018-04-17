9d738964909d95c9e5a61b6a4a0f87e50dafb11d
Walden1995 <sew@jdwalden.com>
2018-04-17 17:00:37 -0500

    modified script

--- Data Integration and Analytics/Task_9.py
+++ Data Integration and Analytics/Task_9.py
@@ -1,3 +1,9 @@
+#pip3 install pandas  This will not work without pandas!
+
+#This file takes data from the Business_Licenses.csv (city of chicago database link below)
+#https://data.cityofchicago.org/Community-Economic-Development/Business-Licenses/r5kz-chrr
+#And from the Crime_data.csv (city of chicago database link below)
+#https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present/ijzp-q8t2
 
 import csv
 import sqlite3
@@ -24,9 +30,17 @@ b = conn.cursor()
 b.execute('SELECT count(DISTINCT legal_name) as num_businesses, ward_precinct FROM businesses WHERE license_code >= 1470 and license_code <= 1477 and license_code != 1472 GROUP BY ward_precinct')
 businesses = b.fetchall()
 
+output = pandas.DataFrame(columns=['block#','#business_with_liquor','#crimes','#arrests'])
+outputindx = 0
 for row in businesses:
   c = conn.cursor()
   if row['ward_precinct'] != None:
     c.execute('''SELECT count(case_number) as num_crimes, count(arrest) as num_arrests FROM crimes WHERE substr(district,2,2)||CAST(ward AS TEXT) = ?''',(row['ward_precinct'].replace("-",""), ))
     crime = c.fetchone()
-    print('{},{},{},{}'.format(row['ward_precinct'].replace("-",""),row['num_businesses'],crime['num_crimes'],crime['num_arrests']))
+    outputindx += 1
+    output.loc[outputindx] = [row['ward_precinct'].replace("-",""),row['num_businesses'],crime['num_crimes'],crime['num_arrests']]
+
+conn.close()
+output.to_csv("Task_9.csv")
+
+print("Look for the Task_9.csv file in the working directory")
